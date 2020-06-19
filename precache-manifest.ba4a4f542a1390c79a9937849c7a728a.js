ff50777a9f63d87d51e2a415d42b6bf49a02f30d
Varun Jain <varunvj181@gmail.com>
2020-06-20 04:59:41 +0530

    Updates

---
+++ precache-manifest.ba4a4f542a1390c79a9937849c7a728a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a0c695d4a82ed4c0e8e73047b1ea4c",
+    "url": "/Image-Search-App/index.html"
+  },
+  {
+    "revision": "8d1b08bda14d41003e7f",
+    "url": "/Image-Search-App/static/css/main.75cb2db5.chunk.css"
+  },
+  {
+    "revision": "4803a25029b11647689e",
+    "url": "/Image-Search-App/static/js/2.c73d4bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Search-App/static/js/2.c73d4bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1b08bda14d41003e7f",
+    "url": "/Image-Search-App/static/js/main.802ef547.chunk.js"
+  },
+  {
+    "revision": "d09f18ffd051b9964cad",
+    "url": "/Image-Search-App/static/js/runtime-main.cdb70f7c.js"
+  }
+]);
